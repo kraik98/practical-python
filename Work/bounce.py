69bc144a13f52d8c4a5d77da18dff8f8bd9c5304
kristopher.raik <kraik1998@gmail.com>
2023-08-02 16:30:44 +0300

    sdf

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,20 @@
 # bounce.py
 #
 # Exercise 1.5
+import pandas as pd
+
+drop_height = 100
+bounce_factor = 3/5
+count = 0
+bouncing_data = []
+
+while count < 10:
+    count += 1
+    bouncing_height = drop_height*bounce_factor
+    bouncing_data.append('{:.4f}'.format(bouncing_height).rstrip('0').rstrip('.'))
+    drop_height = bouncing_height
+
+bouncing_data = pd.DataFrame(bouncing_data)
+bouncing_data = round(bouncing_data, 4).astype('float')
+bouncing_data.index = range(1, len(bouncing_data) + 1)
+print(bouncing_data)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+# pays extra 1000$/month on these months
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+months = 0
+
+while principal > 0:
+    if extra_payment_start_month <= months <= extra_payment_end_month:
+        extra_payment = 1000
+    else:
+        extra_payment = 0
+    principal = principal * (1 + rate / 12) - payment - extra_payment
+    total_paid = total_paid + payment + extra_payment
+    months += 1
+    print(months, total_paid, round(principal,2))
+
+print('Total paid', total_paid)
+print('Total months', months)
