be9f0304ec55ff847b93db0e2818d1976141b07e
kristopher.raik <kraik1998@gmail.com>
2023-08-03 16:37:09 +0300

    sdf

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -11,17 +11,22 @@ total_paid = 0.0
 extra_payment_start_month = 61
 extra_payment_end_month = 108
 extra_payment = 1000
-months = 1
+months = 0
 
 while principal > 0:
+    months += 1
     if extra_payment_start_month <= months <= extra_payment_end_month:
         extra_payment = 1000
     else:
         extra_payment = 0
+    if principal*(1+rate/12) < (payment + extra_payment):
+        payment = principal*(1+rate/12)
+        extra_payment = 0
     principal = principal * (1 + rate / 12) - payment - extra_payment
+
     total_paid = total_paid + payment + extra_payment
-    print(months, round(total_paid, 2), round(principal,2))
-    months += 1
+    print(months, round(total_paid, 2), round(principal, 2))
+
 
 print('Total paid', total_paid)
-print('Total months', months -1)
+print('Total months', months)
